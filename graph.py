de7eea3d2bbc47c5aa7f90ba2232c7374fda2d20
Terminal Tyrant <kubaska05@gmail.com>
2022-11-08 14:30:06 -0700

    framwork

--- graph.py
+++ graph.py
@@ -1,17 +1,13 @@
-
-
-
 class Graph:
     class Node:
         def __init__(self, val) -> None:
-            self.id
-            self.val
+            self.val = val
+            self.lines: list[Graph.Line] = []
+
     class Line:
-        def __init__(self) -> None:
-            self.val: any
-            self.start: Graph.Node
-            self.end: Graph.Node
-            
+        def __init__(self, val, to) -> None:
+            self.val = val
+            self.to: Graph.Node = to
+
     def __init__(self) -> None:
-        self.nodes
-        self.lines
+        self.nodes: dict[str : Graph.Node]
