e0a81ec3e5e8ed39edf72a471f356cdd6e23e5fe
Terminal Tyrant <kubaska05@gmail.com>
2022-11-08 13:57:08 -0700

    first commit

---
+++ graph.py
@@ -0,0 +1,17 @@
+
+
+
+class Graph:
+    class Node:
+        def __init__(self, val) -> None:
+            self.id
+            self.val
+    class Line:
+        def __init__(self) -> None:
+            self.val: any
+            self.start: Graph.Node
+            self.end: Graph.Node
+            
+    def __init__(self) -> None:
+        self.nodes
+        self.lines
