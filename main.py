d9737b54c8d48474abaf0b34ed3b85c8011ea3e0
Terminal Tyrant <kubaska05@gmail.com>
2022-11-10 13:46:22 -0700

    idk what i did

--- main.py
+++ main.py
@@ -2,16 +2,27 @@ from graph import Graph
 
 g = Graph()
 
-g.add_node("Input0", 0.2)
-g.add_node("Input1", 0.4)
-g.add_node("Hidden0", 0.1)
-g.add_node("Hidden1", 0.02)
+# g.add_node("Input0", 0.2)
+# g.add_node("Input1", 0.4)
+# g.add_node("Hidden0", 0.1)
+# g.add_node("Hidden1", 0.02)
 
-g.add_line("Input0", "Hidden0", 0)
-g.add_line("Input0", "Hidden1", 1)
-g.add_line("Input1", "Hidden0", 2)
-g.add_line("Input1", "Hidden0", 3)
+# g.add_line("Input0", "Hidden0", 0)
+# g.add_line("Input0", "Hidden1", 1)
+# g.add_line("Input1", "Hidden0", 2)
+# g.add_line("Input1", "Hidden1", 3)
+
+g.add_node("1", 1)
+g.add_node("2", 2)
+
+g.add_line("1", "2", 3)
+g.add_line("2", "1", 4)
 
 
-for i in g.nodes:
-    print(i)
+for n in g.nodes:
+    print(
+        f"Node {n} with value {g.nodes[n].val} connects to nodes with value: ", end=""
+    )
+    for l in g.nodes[n].lines:
+        print(f"{l.to.val} by line with value {l.val}", end=", ")
+    print()
